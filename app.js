0389e5e80f0260268a297d001538eab5143181e1
JohaNs12234 <Jpp48322@gmail.com>
2025-02-01 12:32:40 -0500

    refactor: el codigo de app.js para que sea mas limpio

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 var amigos = []
-function agregarAmigo () {
-    let amigo = document.getElementById ('amigo').value
- console.log(amigo);
- if (amigo===''||amigo===' '){
-    alert ("Debes ingresar un nombre valido")
-    return
- }
- amigos.push (amigo)
- let nuevoAmigo = document.createElement ('li')
- nuevoAmigo.textContent = amigo
- document.getElementById ('listaAmigos').appendChild(nuevoAmigo)
- document.getElementById('amigo').value = ''
- console.log (amigos)
+
+function agregarAmigo() {
+
+    let amigo = document.getElementById('amigo').value
+
+    if (amigo === '' || amigo === ' ') {
+        alert("Debes ingresar un nombre valido")
+        return
+    }
+
+    amigos.push(amigo)
+
+    let nuevoAmigo = document.createElement('li')
+    nuevoAmigo.textContent = amigo
+    document.getElementById('listaAmigos').appendChild(nuevoAmigo)
+    document.getElementById('amigo').value = ''
 
 }
-function sortearAmigo () {
-    if (amigos.length<=0){
-        alert ("No hay amigos en la lista")
+
+function sortearAmigo() {
+
+    if (amigos.length <= 0) {
+        alert("No hay amigos en la lista")
     }
-    let index = Math.floor ( Math.random ()*amigos.length)
-    console.log(index);
-    console.log(amigos);
-    
-    let amigoSecreto = document.createElement ('li')
-    amigoSecreto.textContent = amigos [index]
-    let resultado = document.getElementById ('resultado')
-    if (resultado.children.length>0) {
-    resultado.replaceChild(amigoSecreto,resultado.children[0])    
-    }else {
-        resultado.appendChild (amigoSecreto)
+
+    let index = Math.floor(Math.random() * amigos.length)
+    let amigoSecreto = document.createElement('li')
+    amigoSecreto.textContent = amigos[index]
+    let resultado = document.getElementById('resultado')
+
+    if (resultado.children.length > 0) {
+        resultado.replaceChild(amigoSecreto, resultado.children[0])
+    } else {
+        resultado.appendChild(amigoSecreto)
     }
 
 }
